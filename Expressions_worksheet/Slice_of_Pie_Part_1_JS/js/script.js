742bcfb01eac8349fe4f808785504f4c5c017602
James Lavender <jamesdlavender@gmail.com>
2013-05-16 19:57:51 -0400

    Added in problem for slices of pie part 1
    Added in problem for slices of pie part 1

---
+++ Expressions_worksheet/Slice_of_Pie_Part_1_JS/js/script.js
@@ -0,0 +1,13 @@
+// Name: James Lavender
+// 5/16/2013
+// Expression Worksheet
+// Slice Of Pie Part 1
+
+// JavaScript
+
+var slicesPizza = 12; // Slices per pizza which is 12.
+var peopleParty = 30; // People at the party which is 30.
+var pizzaOrdered = 4; // Pizzas that were ordered which is 4.
+
+var answer = (pizzaOrdered * slicesPizza)/peopleParty; // Pieces per person.
+console.log(answer); // Answer is 1.6
---
+++ Expressions_worksheet/Slice_of_Pie_Part_2_JS/js/script.js
@@ -0,0 +1,13 @@
+// Name: James Lavender
+// 5/16/2013
+// Expression Worksheet
+
+// JavaScript
+
+var actualAge = 2 // Sparky's actual age.
+var dogAge = 7 // Dogs age 7 time faster than humans.
+
+// Sparky's age in dog years.
+var ageAnswer = actualAge * dogAge
+
+console.log(ageAnswer);
