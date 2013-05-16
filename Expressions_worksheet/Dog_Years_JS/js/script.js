9a35103460facbd27dff3fd4847b134215c0ce3a
James Lavender <jamesdlavender@gmail.com>
2013-05-16 19:59:55 -0400

    Updated Dog Years
    Updated Dog Years.

--- Expressions_worksheet/Dog_Years_JS/js/script.js
+++ Expressions_worksheet/Dog_Years_JS/js/script.js
@@ -1,13 +1,14 @@
 // Name: James Lavender
 // 5/16/2013
 // Expression Worksheet
+// Dog Years
 
 // JavaScript
 
-var actualAge = 2 // Sparky's actual age.
-var dogAge = 7 // Dogs age 7 time faster than humans.
+var actualAge = 2; // Sparky's actual age which is 2.
+var dogAge = 7; // Dogs age 7 time faster than humans.
 
 // Sparky's age in dog years.
-var ageAnswer = actualAge * dogAge
+var ageAnswer = actualAge * dogAge; //Answer to how old is Sparky in dog years.
 
-console.log(ageAnswer);
+console.log(ageAnswer); // Answer is 14.
