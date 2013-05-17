823864bf8bbef4aa054b5a94a279ffecfa6c2667
James Lavender <jamesdlavender@gmail.com>
2013-05-16 21:47:01 -0400

    Added Wacky Assignment.

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,15 @@
+// Name: James Lavender
+// 5/16/2013
+// Assignment: Expressions
+// Wacky
+
+// JavaScript
+
+var clownsBox = prompt ("We are calculating the number of clowns total. \nHow many clowns are in each box?"); // This is the number of clown in each box.
+var numberBoxs = prompt ("How many boxes are there total?"); // This is the number of boxes present.
+
+var totalClowns = clownsBox * numberBoxs; // This is the total amount of clowns.
+
+var result = "There are " + totalClowns + " in all " + numberBoxs + " boxes."; // This is result of total clowns to the user.
+
+alert(result); // This is stating and alerting the user of how many clown there are total.
