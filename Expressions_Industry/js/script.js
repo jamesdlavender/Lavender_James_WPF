c21c4e43f4c21b8ab0fc2429a7dcce9777fd7713
James Lavender <jamesdlavender@gmail.com>
2013-05-16 22:37:49 -0400

    Added Industry Expression.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,31 +1,16 @@
 // Name: James Lavender
 // 5/16/2013
-// Expression Worksheet
-// Discounts
+// Assignment: Expressions
+// Industry
 
 // JavaScript
 
-var itemName = "Speakers"
+var height = prompt ("We are calculating the volume of a skyscraper. \nPlease enter the height of a skyscraper."); // This is the height of the skyscraper.
+var width = prompt ("Please enter the width of a skyscraper."); // This is the width of the skyscraper.
+var depth = prompt ("Please enter the depth of a skyscraper."); // This is the depth of the skyscraper.
 
-var originalPrice = 87.99
-var discountPercent = 0.35
-var salesTax = 0.5
+var totalVolume = height * width * depth; // This is the problem that calculates the Volume of the skyscraper.
 
-var withoutTax = originalPrice - (originalPrice * discountPercent);
+var result = "The volume of the skyscraper is " + totalVolume + " cubic inches."; // This is line adds the text and the area of the skyscraper together
 
-var withTax = withoutTax + (withoutTax * salesTax)
-
-// Sentence structure.
-
-var firstPart = "Your"
-var secondPart = "were originally $"
-var thirdPart = ", but after a"
-var forthPart = "% discount, they are now $"
-var fifthPart = "without tax, and $"
-var sixthPart = "with tax."
-
-// Full statement.
-
-var fullSentence = firstPart + " " + itemName + " " + secondPart + originalPrice + thirdPart + " " + discountPercent + forthPart + withoutTax + " " + fifthPart + withTax + " " + sixthPart 
-
-console.log(fullSentence);
+alert(result); // This states the area of the skyscraper and alerts the user.
