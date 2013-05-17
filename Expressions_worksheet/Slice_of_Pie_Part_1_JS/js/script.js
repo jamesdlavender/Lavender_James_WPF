de4c114d7945fbfa219facb8dc4d95e6ffd3339a
James Lavender <jamesdlavender@gmail.com>
2013-05-16 20:40:55 -0400

    Updated Part 1 Slice of Pie.
    Updated Part 1 Slice of Pie.

--- Expressions_worksheet/Slice_of_Pie_Part_1_JS/js/script.js
+++ Expressions_worksheet/Slice_of_Pie_Part_1_JS/js/script.js
@@ -9,8 +9,8 @@ var slicesPizza = 12; // Slices per pizza which is 12.
 var peopleParty = 30; // People at the party which is 30.
 var pizzaOrdered = 4; // Pizzas that were ordered which is 4.
 
-var firstPart = "Each person ate"
-var lastPart = "slices of pizza at the party."
+var firstPart = "Each person ate" // First part of sentence.
+var lastPart = "slices of pizza at the party." // Last part of sentence.
 
 var answer = (pizzaOrdered * slicesPizza)/peopleParty; // Pieces per person.
 
