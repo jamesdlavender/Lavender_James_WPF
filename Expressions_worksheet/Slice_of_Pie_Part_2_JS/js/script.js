0d572444d4548532a1f4803415e526cd469a7323
James Lavender <jamesdlavender@gmail.com>
2013-05-16 20:39:39 -0400

    Fixed Slice of Pie Part 2.
    Fixed Slice of Pie Part 2.

--- Expressions_worksheet/Slice_of_Pie_Part_2_JS/js/script.js
+++ Expressions_worksheet/Slice_of_Pie_Part_2_JS/js/script.js
@@ -9,5 +9,11 @@ var slicesPizza = 12; // Slices per pizza which is 12.
 var peopleParty = 30; // People at the party which is 30.
 var pizzaOrdered = 4; // Pizzas that were ordered which is 4.
 
+var firstPart = "Sparky got" // First part of sentence.
+var lastPart = "slices of pizza." // Last part of sentence.
+
 var answer = (pizzaOrdered * slicesPizza)%peopleParty; // Remainer pieces of pizza for Sparky.
+
+var fullSentence = firstPart + " " + answer + " " + lastPart; // Full statement with answers.
+
 console.log(answer); // Answer is 18.
