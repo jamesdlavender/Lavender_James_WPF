7800861f8758efb8f85c45686fe09a8c9320b2e0
James Lavender <jamesdlavender@gmail.com>
2013-05-16 22:29:21 -0400

    Added in the Personal Expression.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,15 +1,17 @@
 // Name: James Lavender
 // 5/16/2013
 // Assignment: Expressions
-// Wacky
+// Personal
 
 // JavaScript
 
-var clownsBox = prompt ("We are calculating the number of clowns total. \nHow many clowns are in each box?"); // This is the number of clown in each box.
-var numberBoxs = prompt ("How many boxes are there total?"); // This is the number of boxes present.
+var height = prompt ("We are calculating the area of a triangle table. \nPlease enter the height of the triangle table."); // This is the height of the triangle.
+var width = prompt ("Please enter the width of the triangle table."); // This is the width of the triangle.
 
-var totalClowns = clownsBox * numberBoxs; // This is the total amount of clowns.
+var totalArea = height * width; // This is the total area of the triangle without dividing by 2.
 
-var result = "There are " + totalClowns + " in all " + numberBoxs + " boxes."; // This is result of total clowns to the user.
+var triangleArea = totalArea / 2; //  This is the actual area of the triangle.
 
-alert(result); // This is stating and alerting the user of how many clown there are total.
+var result = "The area of the triangle is " + triangleArea; // This is line adds the text and the area of the triangle together.
+
+alert(result); // This states the area of the triangle and alerts the user.
