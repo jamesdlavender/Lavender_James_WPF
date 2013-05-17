0e9d667c26b307ca5ae430e8f61e1e34b8e96922
James Lavender <jamesdlavender@gmail.com>
2013-05-16 20:06:17 -0400

    Added Part 2 of Slice of Pie.
    Added in the variables for this problem.

--- Expressions_worksheet/Slice_of_Pie_Part_2_JS/js/script.js
+++ Expressions_worksheet/Slice_of_Pie_Part_2_JS/js/script.js
@@ -1,13 +1,13 @@
 // Name: James Lavender
 // 5/16/2013
 // Expression Worksheet
+// Slice Of Pie Part 2
 
 // JavaScript
 
-var actualAge = 2 // Sparky's actual age.
-var dogAge = 7 // Dogs age 7 time faster than humans.
+var slicesPizza = 12; // Slices per pizza which is 12.
+var peopleParty = 30; // People at the party which is 30.
+var pizzaOrdered = 4; // Pizzas that were ordered which is 4.
 
-// Sparky's age in dog years.
-var ageAnswer = actualAge * dogAge
-
-console.log(ageAnswer);
+var answer = (pizzaOrdered * slicesPizza)%peopleParty; // Pieces per person.
+console.log(answer); // Answer is 
