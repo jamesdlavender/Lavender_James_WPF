b1c2585316fe2433ec96f5f4cc60ff78dd794b3b
James Lavender <jamesdlavender@gmail.com>
2013-05-16 20:33:12 -0400

    Fixed Dog years.
    Fixed Dog years.

--- Expressions_worksheet/Dog_Years_JS/js/script.js
+++ Expressions_worksheet/Dog_Years_JS/js/script.js
@@ -7,8 +7,13 @@
 
 var actualAge = 2; // Sparky's actual age which is 2.
 var dogAge = 7; // Dogs age 7 time faster than humans.
+var firstPart = "Sparky is" // First part of sentence.
+var middlePart = "human years old which is" // Middle part of sentence.
+var lastPart = "in dog years." // Last part of snetence.
 
 // Sparky's age in dog years.
 var ageAnswer = actualAge * dogAge; //Answer to how old is Sparky in dog years.
 
-console.log(ageAnswer); // Answer is 14.
+var fullSentence = firstPart + " " + actualAge + " " + middlePart + " " + ageAnswer + " " + lastPart; // Entire statement with answers.
+
+console.log(fullSentence);
