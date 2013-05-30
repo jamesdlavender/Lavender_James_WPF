49831924150592ffeb87aad9992883f34f7af083
James Lavender <jamesdlavender@gmail.com>
2013-05-29 20:22:17 -0700

    Updated Stung Problem.

--- Functions_worksheet/Stung/js/script.js
+++ Functions_worksheet/Stung/js/script.js
@@ -1,31 +1,6 @@
 // Name: James Lavender
 // 5/16/2013
-// Expression Worksheet
-// Discounts
+// Functions Worksheet
+// Stung!
 
 // JavaScript
-
-var itemName = "Speakers"
-
-var originalPrice = 87.99
-var discountPercent = 0.35
-var salesTax = 0.5
-
-var withoutTax = originalPrice - (originalPrice * discountPercent);
-
-var withTax = withoutTax + (withoutTax * salesTax)
-
-// Sentence structure.
-
-var firstPart = "Your"
-var secondPart = "were originally $"
-var thirdPart = ", but after a"
-var forthPart = "% discount, they are now $"
-var fifthPart = "without tax, and $"
-var sixthPart = "with tax."
-
-// Full statement.
-
-var fullSentence = firstPart + " " + itemName + " " + secondPart + originalPrice + thirdPart + " " + discountPercent + forthPart + withoutTax + " " + fifthPart + withTax + " " + sixthPart 
-
-console.log(fullSentence);
