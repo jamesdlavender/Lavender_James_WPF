3f20e2ca92d4fd8eca23770290fc871f8b627d01
James Lavender <jamesdlavender@gmail.com>
2013-05-29 23:34:29 -0700

    Updated comments.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -7,9 +7,9 @@
 
 // Volume of an Office Building.
 
-var width = prompt ("Please enter the width of the building.");
-var height = prompt ("Please enter the height of the building.");
-var depth = prompt ("Please enter the depth of the building.");
+var width = prompt ("Please enter the width of the building."); // Width of building.
+var height = prompt ("Please enter the height of the building."); // Height of building.
+var depth = prompt ("Please enter the depth of the building."); // Depth of building
 
 // Anonymous function, and has 3 operators.
 var calcVolume = function(width, height, depth){ // Defining the function.
@@ -21,4 +21,4 @@ var a = calcVolume(width, height, depth); // This invokes the function.
 
 //(var <= var) ? alert() : alert();
 
-alert("The volume of the build is " + a + " cubic inches. \n\nThe width was " + width + " inches, height was " + height + " inches, and the depth was " + depth + " inches."); // This outputs to console.
+alert("The volume of the build is " + a + " cubic inches. \n\nThe width was " + width + " inches, height was " + height + " inches, and the depth was " + depth + " inches."); // This outputs to alert.
