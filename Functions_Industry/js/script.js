6bdacb3b3f7dd1f100db07ad4fdf858b5c53264b
James Lavender <jamesdlavender@gmail.com>
2013-05-29 22:44:58 -0700

    Updated alert once more.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -20,4 +20,4 @@ var a = calcVolume(width, height, depth); // This invokes the function.
 
 //(var <= var) ? alert() : alert();
 
-alert("The volume of the build is " + a + " cubic inches. The width was " + width + " inches, height was " + height + " inches, and the depth was " + depth + " inches."); // This outputs to console.
+alert("The volume of the build is " + a + " cubic inches. \n\nThe width was " + width + " inches, height was " + height + " inches, and the depth was " + depth + " inches."); // This outputs to console.
