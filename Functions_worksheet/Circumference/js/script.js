e96eb23261e9dd0c09fe5dea14db65161447df9d
James Lavender <jamesdlavender@gmail.com>
2013-05-29 20:07:11 -0700

    Added circumference problem.

--- Functions_worksheet/Circumference/js/script.js
+++ Functions_worksheet/Circumference/js/script.js
@@ -1,31 +1,16 @@
 // Name: James Lavender
 // 5/16/2013
-// Expression Worksheet
-// Discounts
+// Functions Worksheet
+// Circumference
 
 // JavaScript
 
-var itemName = "Speakers"
+var radius = 7;
+var pi = 3.14;
 
-var originalPrice = 87.99
-var discountPercent = 0.35
-var salesTax = 0.5
+var circumference =calculateCirle(radius, pi);
+console.log("The circumference of the circle is " + circumference);
 
-var withoutTax = originalPrice - (originalPrice * discountPercent);
-
-var withTax = withoutTax + (withoutTax * salesTax)
-
-// Sentence structure.
-
-var firstPart = "Your"
-var secondPart = "were originally $"
-var thirdPart = ", but after a"
-var forthPart = "% discount, they are now $"
-var fifthPart = "without tax, and $"
-var sixthPart = "with tax."
-
-// Full statement.
-
-var fullSentence = firstPart + " " + itemName + " " + secondPart + originalPrice + thirdPart + " " + discountPercent + forthPart + withoutTax + " " + fifthPart + withTax + " " + sixthPart 
-
-console.log(fullSentence);
+private function calculateCirle(r, p){
+	return r*p;
+}
