9d22f5cd09bdc4c5933c8cdb0db229da414ab15b
James Lavender <jamesdlavender@gmail.com>
2013-05-29 20:17:32 -0700

    Added comments to Circumference Problem.

--- Functions_worksheet/Circumference/js/script.js
+++ Functions_worksheet/Circumference/js/script.js
@@ -5,10 +5,11 @@
 
 // JavaScript
 
-var radius = 7;
-var pi = 3.14;
+var radius = 7; // Radius of the circle is 7.
+var pi = 3.14; // Pi is required to find the circumference of a circle.
 
 var calcCircle = function(radius, pi){
+	// This calculates the circumference of a circle by multiplying.
 	var circumference = radius * pi;
 	return circumference;
 }
