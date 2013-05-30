081f5bd5eedc04339e0f3680c0cb72a9c97ddd39
James Lavender <jamesdlavender@gmail.com>
2013-05-29 20:51:55 -0700

    Added a comment to line 13.

--- Functions_worksheet/Circumference/js/script.js
+++ Functions_worksheet/Circumference/js/script.js
@@ -10,7 +10,7 @@
 var radius = 7; // Radius of the circle is 7.
 var pi = 3.14; // Pi is required to find the circumference of a circle.
 
-var calcCircle = function(radius, pi){
+var calcCircle = function(radius, pi){ // Defining the function.
 	// This calculates the circumference of a circle by multiplying.
 	var circumference = radius * pi; // This part does the actual multiplication.
 	return circumference; // Returns the circumference of the circle.
