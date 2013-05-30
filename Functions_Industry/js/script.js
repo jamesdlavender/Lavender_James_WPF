a657c85775ac769c3f712f21e946aa92d94ab87b
James Lavender <jamesdlavender@gmail.com>
2013-05-29 20:59:14 -0700

    Prepped Industry Problem.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,16 +1,7 @@
 // Name: James Lavender
-// 5/16/2013
-// Assignment: Expressions
+// 5/29/2013
+// Assignment: Functions
 // Industry
 
 // JavaScript
 
-var height = prompt ("We are calculating the volume of a skyscraper. \nPlease enter the height of a skyscraper."); // This is the height of the skyscraper.
-var width = prompt ("Please enter the width of a skyscraper."); // This is the width of the skyscraper.
-var depth = prompt ("Please enter the depth of a skyscraper."); // This is the depth of the skyscraper.
-
-var totalVolume = height * width * depth; // This is the problem that calculates the Volume of the skyscraper.
-
-var result = "The volume of the skyscraper is " + totalVolume + " cubic inches."; // This is line adds the text and the area of the skyscraper together
-
-alert(result); // This states the area of the skyscraper and alerts the user.
