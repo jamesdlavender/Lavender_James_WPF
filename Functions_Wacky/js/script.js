8acaed4db030451c6309fb8a6465265a1e87b090
James Lavender <jamesdlavender@gmail.com>
2013-05-30 00:47:30 -0700

    Finished Wacky Problem.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -4,3 +4,19 @@
 // Wacky
 
 // JavaScript
+
+// 
+var gumSticks = prompt("How many pieces of gum are in a pack?")
+var gumPacks = prompt("How many packs of gum?")
+var gumChews = 100;
+var maxChews = 30000;
+
+var calcChews = function(gumSticks, gumPacks, gumChews){ // Defining the function. Anonymous function.
+	var total = gumSticks * gumPacks * gumChews; // The actual multiplying.
+	return total;
+}
+
+var totalChews = calcChews(gumSticks, gumPacks, gumChews); // Named function.
+
+
+(totalChews <= maxChews) ? alert("Your jaw is safe! You didn't chew too much! :]") : alert("Your jaw is about to fall off, you chewed to much! :["); // This determinds if the user went over the budget or not.
