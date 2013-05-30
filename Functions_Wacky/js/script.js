940b5ce4543320ee86a0304e0431a863cb6a84ea
James Lavender <jamesdlavender@gmail.com>
2013-05-30 00:51:18 -0700

    Added comments.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,10 +6,10 @@
 // JavaScript
 
 // 
-var gumSticks = prompt("How many pieces of gum are in a pack?");
-var gumPacks = prompt("How many packs of gum?");
-var gumChews = 100;
-var maxChews = 30000;
+var gumSticks = prompt("How many pieces of gum are in a pack?"); // Prompts user to add the amount of gun in a pack.
+var gumPacks = prompt("How many packs of gum?"); // Prompts user how many packs of gum.
+var gumChews = 100; // How many chews are in one piece of gum.
+var maxChews = 30000; // Max amount of gun until your jaw falls off.
 
 var calcChews = function(gumSticks, gumPacks, gumChews){ // Defining the function. Anonymous function.
 	var total = gumSticks * gumPacks * gumChews; // The actual multiplying.
@@ -18,5 +18,5 @@ var calcChews = function(gumSticks, gumPacks, gumChews){ // Defining the functio
 
 var totalChews = calcChews(gumSticks, gumPacks, gumChews); // Named function.
 
-
-(totalChews <= maxChews) ? alert("Your jaw is safe! You didn't chew too much! :]") : alert("Your jaw is about to fall off, you chewed to much! :[");
+// Ternary
+(totalChews <= maxChews) ? alert("Your jaw is safe! You didn't chew too much! :]") : alert("Your jaw is about to fall off, you chewed to much! :["); // This determinds if you chewed to much.
