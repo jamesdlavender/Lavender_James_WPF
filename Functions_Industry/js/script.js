cf8c2e884b67b04a487bd82ec7844c07a088be9e
James Lavender <jamesdlavender@gmail.com>
2013-05-29 21:52:57 -0700

    Added Industry Problem.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,3 +5,19 @@
 
 // JavaScript
 
+// Volume of an Office Building.
+
+var width = prompt ("Please enter the width of the building.");
+var height = prompt ("Please enter the height of the building.");
+var depth = promt ("Please enter the depth of the building.");
+
+var calcVolume = function(width, height, depth){ // Defining the function.
+	var volume = width * height * depth;
+	return volume;
+}
+
+var a = calcVolume(width, height, depth); // This invokes the function.
+
+//(var <= var) ? alert() : alert();
+
+console.log(a); // This outputs to console.
