a6b72234ebbb2dca433d8f3ed2024835f64ad015
James Lavender <jamesdlavender@gmail.com>
2013-05-29 20:28:55 -0700

    Testing Stung Problem.

--- Functions_worksheet/Stung/js/script.js
+++ Functions_worksheet/Stung/js/script.js
@@ -4,3 +4,15 @@
 // Stung!
 
 // JavaScript
+
+var beeStings = 8.666666667;
+var animalPounds = 400;
+
+var calcStings = function(beeStings, animalPounds){
+	var overallStings = beeStings * animalPounds;
+	return overallStings;
+}
+
+var a = calcStings(8.666666667, 400);
+
+console.log(a);
